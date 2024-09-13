138097186ad34e15a791dd83af3c7a10d366e8ef
Laurence Luo <admin@lzc.app>
2024-09-13 15:05:46 +0800

    KindleMate2_WPF

--- KindleMate2_WPF/App.xaml.cs
+++ KindleMate2_WPF/App.xaml.cs
@@ -7,10 +7,9 @@ using System.Windows;
 
 namespace KindleMate2_WPF
 {
-    /// <summary>
-    /// App.xaml 的交互逻辑
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+	/// <summary>
+	/// App.xaml 的交互逻辑
+	/// </summary>
+	public partial class App : Application
+	{ }
 }
--- KindleMate2_WPF/Properties/AssemblyInfo.cs
+++ KindleMate2_WPF/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KindleMate2_WPF")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © RainySummer  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,13 +31,12 @@ using System.Windows;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //主题特定资源词典所处位置
-                                     //(未在页面中找到资源时使用，
-                                     //或应用程序资源字典中找到时使用)
-    ResourceDictionaryLocation.SourceAssembly //常规资源词典所处位置
-                                              //(未在页面中找到资源时使用，
-                                              //、应用程序或任何主题专用资源字典中找到时使用)
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, //主题特定资源词典所处位置
+	//(未在页面中找到资源时使用，
+	//或应用程序资源字典中找到时使用)
+	ResourceDictionaryLocation.SourceAssembly //常规资源词典所处位置
+	//(未在页面中找到资源时使用，
+	//、应用程序或任何主题专用资源字典中找到时使用)
 )]
 
 
